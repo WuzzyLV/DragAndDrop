1e57e19ca8909073e72ce2a0a1ab8cd3ad5464b7
Reinis <reinisemilsa@gmail.com>
2023-06-12 02:48:10 +0300

    Adding counting abilities

--- Assets/Skripti/Objekti.cs
+++ Assets/Skripti/Objekti.cs
@@ -3,22 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Objekti : MonoBehaviour {
-	public GameObject atkritumuMasina;
-	public GameObject atraPalidziba;
-	public GameObject autobuss;
-	
+
+	//Katru reizi kad pacel mašīnu tā pozīcija tiek saglabāta šajā mainīgajā
+	//gadijumā ja to ieliek nepareizejā vietā, lai nebutu vajadziba saglabat katras mašīnas sākuma pozīciju
 	[HideInInspector] 
 	public Vector2 lastPos;
 
 	public Canvas kanva;
+
+	//uzvaras paneļa instance lai to var paradit kad spele beidzas
+	public GameObject WinPanel;
+
+	//Cik masinas ir jasaliek lai spele beigtos
+	public int TotalCarCount;
 	
 	[HideInInspector]
 	public bool vaiIstajaVieta = false;
 
+	[HideInInspector]
 	public GameObject pedejaisVilktais = null;
 
-
-	void Start() {
-		
+	int correctCarCount = 0;
+	
+	public void addCorrectCar()
+	{
+		correctCarCount++;
+		if(correctCarCount == TotalCarCount)
+		{
+			Debug.Log("Spele beigusies");
+			WinPanel.SetActive(true);
+			Timer.Instance.TogglePause();
+		}
 	}
+	
+	public int GetCorrectCarCount()
+	{
+		return correctCarCount;
+	}
+	
 }
